8b18dc1eab488315f7ea9217c264a1c6e0c4a60d
Sakshi Biradar <sakshibiradar995@gmail.com>
2026-07-12 22:54:20 +0530

    Add files via upload

---
+++ program152.cpp
@@ -0,0 +1,56 @@
+//Write a C++ program to accept array elements from the user and calculate the sum of all elements
+#include<iostream>
+using namespace std;
+class ArrayX
+{
+    public:
+    int Summation(int Arr[],int iSize)
+ { 
+    int iCnt = 0;
+    int iSum = 0;
+    for(iCnt = 0;iCnt < iSize;iCnt++)
+    {
+        iSum = iSum+Arr[iCnt];
+    }
+    return iSum;
+
+ }
+   
+};
+
+
+
+int main()
+{
+   
+   int *Brr = NULL;
+   int  iLength = 0,iCnt=0,iRet = 0;
+   ArrayX aobj;
+
+  
+   cout<<"Enter no of Elements :\n";
+   cin>>iLength;
+
+   Brr = new int[iLength];
+   cout<<"Enter Elements :\n";
+   for(iCnt = 0;iCnt < iLength;iCnt++)
+   {
+    cin>>Brr[iCnt];
+
+   }
+   cout<<"Elements of the array are :\n";
+   for(iCnt = 0;iCnt < iLength;iCnt++)
+   {
+    cout<<Brr[iCnt]<<endl;
+
+   }
+
+   iRet = aobj.Summation(Brr,iLength);
+   cout<<"Summation is :"<<iRet<<endl;
+
+   
+   delete[]Brr;
+   
+   return 0;
+
+}
